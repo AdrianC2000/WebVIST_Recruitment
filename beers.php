55b27beb575790468942f14c596b85ddf568b023
AdrianC2000 <adrianciesielczyk2000@gmail.com>
2021-07-25 19:41:25 +0200

    Api and Json handling done, almost finished beers page

--- beers.php
+++ beers.php
@@ -1,13 +1,25 @@
 <?php
 
 $url = 'https://api.punkapi.com/v2/beers';
-$jsonBeers = json_decode(file_get_contents($url));
-$count = count($jsonBeers);
-print $count;
+$json = file_get_contents($url);
+$jsonBeers = json_decode($json);
+$beersCount = count($jsonBeers);
+$subpagesCounter = intval($beersCount / 5);
+
+$jsonBeersByPage = array_chunk($jsonBeers, $subpagesCounter);
+/*var_dump($jsonBeersByPage[0]);*/
+
+$id = 1;
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
 
 echo $twig->render('beers.html.twig', [
     'post' => $_POST,
     'session' => $_SESSION,
     'get' => $_GET,
-    'count' => $count,
-    'beers' => $jsonBeers]);
+    'json' => $json,
+    'count' => $beersCount,
+    'subpagesCounter' => $subpagesCounter,
+    'id' => $id,
+    'beers' => $jsonBeersByPage]);
